1ec8d5aba3851de248075dd5d2582d09c18ad69e
esau <esaufreirematias@gmail.com>
2023-04-16 15:00:53 -0300

    feat - impleemnta compoente search

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -3,14 +3,30 @@ import { ReactSVG } from "react-svg";
 import { AuthorizedLayout } from "../../Components/AuthorizedLayout";
 
 import Vector2 from "../../Assets/Vector.svg";
+import atencao from "../../Assets/atencao.svg";
+import darkIcon from "../../Assets/darkIcon.svg";
+import Search from "../../Assets/Search Icon.svg";
+import Avatar from "../../Assets/Avatar.svg";
+import sinoIcon from "../../Assets/sinoIcon.svg";
 
 import "./Dashboard.css";
 
-
 const Dashboard = () => {
-
   return (
     <AuthorizedLayout>
+      <div className="containerSearch">
+        <div className="containerLeft">
+          <ReactSVG className="iconFilter" src={Search} />
+          <input type="text" placeholder="Buscar"/>
+        </div>
+
+        <div className="containerRight">
+          <ReactSVG className="iconFilter" src={sinoIcon} />
+          <ReactSVG className="iconFilter" src={darkIcon} />
+          <ReactSVG className="iconFilter" src={atencao} />
+          <ReactSVG className="iconFilter" src={Avatar} />
+        </div>
+      </div>
       <div className="subtitle" style={{ marginBottom: 10 }}>
         Principal / Dashboard
       </div>
@@ -19,18 +35,16 @@ const Dashboard = () => {
       <div className="containerFilter">
         <div>Filtrar por data</div>
         <div className="containerDate">
-          <ReactSVG
-            className="iconFilter"
-            src={Vector2}
-          />
-          <div className="subtitle" style={{ colo: '#8F9BBA'}}>Data Inicial</div>
+          <ReactSVG className="iconFilter" src={Vector2} />
+          <div className="subtitle" style={{ colo: "#8F9BBA" }}>
+            Data Inicial
+          </div>
         </div>
         <div className="containerDate">
-          <ReactSVG
-            className="iconFilter"
-            src={Vector2}
-          />
-          <div className="subtitle" style={{ colo: '#8F9BBA'}}>Data Final</div>
+          <ReactSVG className="iconFilter" src={Vector2} />
+          <div className="subtitle" style={{ colo: "#8F9BBA" }}>
+            Data Final
+          </div>
         </div>
       </div>
     </AuthorizedLayout>
